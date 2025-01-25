269d0234f5d26662f3516eda77bbba8e4e54f92c
nbonillav <13660657+nbonillav@users.noreply.gitlab.com>
2025-01-25 14:38:41 -0600

    Small and Medium css for mobile porpuse

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function newParagraph(){
     const section__content = document.querySelector('.section__content');
     const new_p = document.createElement('p');
     new_p.setAttribute('class', 'section__more')
-    new_p.textContent = 'Also, I know a little bit about JavaScript';
+    new_p.textContent = "Also, I'm proficient in HTML5, CSS3, and JavaScript";
 
     return section__content.appendChild(new_p);
 }
